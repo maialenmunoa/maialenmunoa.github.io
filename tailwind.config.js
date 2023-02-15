5eb82ae8e048a3a833ce992686a4e2d1397e406e
Maialen Muñoa <mmunoa@yahoo.com>
2023-02-15 14:36:49 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    '**/*.html',
+    '**/*.md'
+  ],
+  theme: {
+    fontFamily: {
+      'serif': ['"Playfair Display"', 'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      'sans': ['Montserrat', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"'],
+      'hand': ['"Sassy Frass"', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"'],
+    }
+  },
+  plugins: []
+}
