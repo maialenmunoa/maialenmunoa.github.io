426cbf7a71fce5f47bfbf9bb68772af0d4ca2340
Maialen Muñoa <mmunoa@yahoo.com>
2023-05-13 12:02:26 +0200

    web portfolio

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -6,11 +6,18 @@ $(document).ready(function() {
         $("#popup-image").attr("src", imageSrc);
         $("#popup-content").html(postContent);
         $("#popup").removeClass("hidden");
+        $("body").addClass("popup-opened");
     });
 
     $("#popup").click(function(event) {
         if (event.target.id === "popup") {
-        $("#popup").addClass("hidden");
+            $("#popup").addClass("hidden");
+            $("body").removeClass("popup-opened");
         }
+    });
+
+    $("#popup-close").click(function() {
+        $("#popup").addClass("hidden");
+        $("body").removeClass("popup-opened");
     });
 });
