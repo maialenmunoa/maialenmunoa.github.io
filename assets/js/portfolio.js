72e8783dcc01d708dfa2f4ce1039b69dd3819523
maialenmunoa <maialenmunoa@users.noreply.github.com>
2023-05-14 22:53:48 +0000

    deploy: 647a9bc219ffd1f136233d37762fcc98c5819a91

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,10 +1,46 @@
 $(document).ready(function() {
-    $(".portfolio-button").click(function() {
+    /*$(".portfolio-button").click(function() {
         var imageSrc = $(this).attr("data-image-src");
         var postContent = unescape($(this).attr("data-post-content"));
         
         $("#popup-image").attr("src", imageSrc);
+        $("#popup").removeClass("hidden");
+        $("body").addClass("popup-opened");
+    });*/
+
+    $(".portfolio-button").click(function() {
+        // Obtener la lista de imágenes desde el atributo "data-image-src" del botón
+        var imageList = $(this).attr("data-image-src").split(",");
+        
+        // Obtener el contenido desde el atributo "data-post-content" del botón
+        var postContent = unescape($(this).attr("data-post-content"));
+        
+        // Obtener el índice de la imagen actual
+        var currentIndex = 0;
+        
+        // Crear el contenido HTML del carrusel
+        var slickHtml = '<div id="popup-image-carousel" class="slick-carousel">';
+        for (var i = 0; i < imageList.length; i++) {
+            slickHtml += '<div class="slick-slide"><img src="' + imageList[i] + '" alt=""></div>';
+            if (i === 0) {
+            currentIndex = i;
+            }
+        }
+        slickHtml += '</div>';
+        
+        // Agregar el contenido HTML al popup
+        $("#popup-carousel").html(slickHtml);
+        
+        // Inicializar el carrusel
+        $("#popup-image-carousel").slick({
+            dots: true,
+            arrows: true,
+            initialSlide: currentIndex
+        });
+        
         $("#popup-content").html(postContent);
+        
+        // Mostrar el popup
         $("#popup").removeClass("hidden");
         $("body").addClass("popup-opened");
     });
